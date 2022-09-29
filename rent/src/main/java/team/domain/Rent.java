56bc8d1eb67aab6c38b388f48e8cab627c71e680
juno-cha <cherry28j@naver.com>
2022-09-29 07:16:09 +0000

    initialize

--- rent/src/main/java/team/domain/Rent.java
+++ rent/src/main/java/team/domain/Rent.java
@@ -15,78 +15,46 @@ import java.util.Date;
 
 public class Rent  {
 
-    
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
-    
-    
-    
-    
-    
+
     private Long orderId;
-    
-    
-    
-    
     
     private Long productId;
     
-    
-    
-    
-    
     private Date startTime;
-    
-    
-    
-    
     
     private Date endTime;
     
-    
-    
-    
-    
     private Long rentHour;
-    
-    
-    
-    
     
     private Long totalPrice;
 
     @PostPersist
     public void onPostPersist(){
 
-
         Returned returned = new Returned(this);
         returned.publishAfterCommit();
 
-
-
         RentalCanceled rentalCanceled = new RentalCanceled(this);
         rentalCanceled.publishAfterCommit();
-
-
 
         BikeRented bikeRented = new BikeRented(this);
         bikeRented.publishAfterCommit();
 
         // Get request from Management
-        //team.external.Management management =
-        //    Application.applicationContext.getBean(team.external.ManagementService.class)
-        //    .getManagement(/** mapping value needed */);
+        team.external.Management management =
+           RentApplication.applicationContext.getBean(team.external.ManagementService.class)
+           .getManagement(getProductId());
 
+        //Validation Check (Bike already rented.)
+        if(management.getRentAvailable())
+            throw new RuntimeException("The bike is unavailable.");
     }
 
     public static RentRepository repository(){
         RentRepository rentRepository = RentApplication.applicationContext.getBean(RentRepository.class);
         return rentRepository;
     }
-
-
-
-
-
 
 }
